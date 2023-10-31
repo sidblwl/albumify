1be91ca08b096fd4a1f70e5a4ccaaefd72e6a5cf
Siddharth Belwal <siddharth@wireless-10-104-217-218.umd.edu>
2023-10-31 00:36:55 -0400

    changed the host point

--- src/script.js
+++ src/script.js
@@ -59,7 +59,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "http://localhost:80/callback");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
