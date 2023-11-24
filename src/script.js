d25e74e0ea2bcdcd16548b8de5113c2d62c5f4f9
Siddharth Belwal <siddharth@siddharthsmbp3.lan>
2023-11-24 15:33:10 -0800

    changed back to 5173

--- src/script.js
+++ src/script.js
@@ -25,7 +25,7 @@ export async function redirectToAuthCodeFlow(clientId) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:80/callback");
+    params.append("redirect_uri", "http://localhost:5173/callback");
     params.append("scope", "user-read-private user-read-email user-library-read user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -59,7 +59,7 @@ export async function getAccessToken(clientId, code) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:80/callback");
+    params.append("redirect_uri", "http://localhost:5173/callback");
     params.append("code_verifier", verifier);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
